e26d854458c9887d52fd718005455a7b01414342
Junseok Oh <kotohana5706@edcan.kr>
2016-08-11 17:21:47 +0900

    Implement Retrofit Implement Retrofit Implement GsonConverter Implement NetworkHelper

---
+++ app/src/main/java/kr/edworopen/goodreserve/utils/GoodReserveNetworkHelper.java
@@ -0,0 +1,43 @@
+/*
+ * Created by Junseok Oh on 2016.
+ * Copyright by Good-Reserve Project by Sunrin Internet High School EDCAN / IWOP / ZEROPEN
+ * All rights reversed.
+ */
+
+package kr.edworopen.goodreserve.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by KOHA_DESKTOP on 2016. 6. 29..
+ */
+public class GoodReserveNetworkHelper {
+    private Context context;
+    final public static String url = "http://kafuuchino.one";
+    final public static int port = 8000;
+
+    public GoodReserveNetworkHelper(Context context) {
+        this.context = context;
+    }
+
+    public static Retrofit retrofit;
+
+    public static NetworkInterface getNetworkInstance() {
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(url + ":" + port)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(NetworkInterface.class);
+    }
+
+    public static boolean returnNetworkState(Context context) {
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+}
---
+++ app/src/main/java/kr/edworopen/goodreserve/utils/NetworkInterface.java
@@ -0,0 +1,13 @@
+/*
+ * Created by Junseok Oh on 2016.
+ * Copyright by Good-Reserve Project by Sunrin Internet High School EDCAN / IWOP / ZEROPEN
+ * All rights reversed.
+ */
+
+package kr.edworopen.goodreserve.utils;
+
+/**
+ * Created by JunseokOh on 2016. 8. 11..
+ */
+public class NetworkInterface {
+}

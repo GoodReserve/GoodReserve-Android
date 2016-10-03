d39d88312d74185366d707e640c7b737d36d687c
Junseok Oh <kotohana5706@edcan.kr>
2016-10-04 00:14:41 +0900

    [RegisterActivity] Check Password, Email Form

--- app/src/main/java/kr/edcan/rerant/activity/RegisterActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/RegisterActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import kr.edcan.rerant.R;
 import kr.edcan.rerant.databinding.ActivityRegisterBinding;
+import kr.edcan.rerant.utils.StringUtils;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -29,8 +30,18 @@ public class RegisterActivity extends AppCompatActivity {
         binding.registerExecute.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(RegisterActivity.this, "회원가입", Toast.LENGTH_SHORT).show();
+                checkRegister();
             }
         });
+    }
+
+    private void checkRegister() {
+        if (!StringUtils.validEmail(binding.registerEmail)) {
+            Toast.makeText(this, "올바른 이메일 형식을 입력해주세요!", Toast.LENGTH_SHORT).show();
+        } else if(!StringUtils.checkPassword(binding.registerPassword, binding.registerRePassword)){
+            Toast.makeText(this, "비밀번호가 일치하지 않습니다!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "회원가입", Toast.LENGTH_SHORT).show();
+        }
     }
 }

8ecadd58ec136b01e2cff452cfcafcf25b4f053f
Junseok Oh <kotohana5706@edcan.kr>
2016-10-04 00:11:46 +0900

    [StringUtils] Implement Methods to Check @RegisterActivity

--- app/src/main/java/kr/edcan/rerant/utils/StringUtils.java
+++ app/src/main/java/kr/edcan/rerant/utils/StringUtils.java
@@ -1,8 +1,16 @@
 package kr.edcan.rerant.utils;
 
+import android.widget.EditText;
+
 /**
  * Created by Junseok on 2016-10-04.
  */
 
 public class StringUtils {
+    public static boolean validEmail(EditText editText){
+        return editText.getText().toString().trim().contains("@");
+    }
+    public static boolean checkPassword(EditText password, EditText repassword){
+        return password.getText().toString().trim().equals(repassword.getText().toString().trim());
+    }
 }

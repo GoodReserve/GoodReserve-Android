14a157fe179c489a2d69d454751c74702b00e4e1
Junseok Oh <kotohana5706@edcan.kr>
2016-10-03 19:10:50 +0900

    [MainActivity] Configure Placeholder UI

--- app/src/main/java/kr/edcan/rerant/activity/MainActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/MainActivity.java
@@ -7,16 +7,37 @@
 package kr.edcan.rerant.activity;
 
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import kr.edcan.rerant.R;
+import kr.edcan.rerant.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSupportActionBar(binding.toolbar);
+        getSupportActionBar().setTitle("");
+        binding.progressLoading.startAnimation();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                initUI();
+            }
+        }, 2000);
+    }
+    private void initUI(){
+        getSupportActionBar().setTitle("GoodReserve");
+        binding.progressComplete.setVisibility(View.VISIBLE);
+        binding.progressLoading.setVisibility(View.GONE);
+        binding.toolbar.setTitleTextColor(Color.WHITE);
     }
 }

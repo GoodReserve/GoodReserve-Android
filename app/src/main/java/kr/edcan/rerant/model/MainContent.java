39b726c4a16f4c42a7053ac3a3c8d0141f1ed01d
Junseok Oh <kotohana5706@edcan.kr>
2016-10-06 09:42:11 +0900

    [Models] Implement Content

---
+++ app/src/main/java/kr/edcan/rerant/model/MainContent.java
@@ -0,0 +1,29 @@
+package kr.edcan.rerant.model;
+
+/**
+ * Created by JunseokOh on 2016. 10. 6..
+ */
+public class MainContent {
+    String code, resTitle, leftTime;
+
+    public MainContent(String code, String resTitle, String leftTime) {
+        this.code = code;
+        this.resTitle = resTitle;
+        this.leftTime = leftTime;
+    }
+
+    public MainContent() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getResTitle() {
+        return resTitle;
+    }
+
+    public String getLeftTime() {
+        return leftTime;
+    }
+}

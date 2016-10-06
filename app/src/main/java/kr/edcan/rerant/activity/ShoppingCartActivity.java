8a7070ee6f79c7b833195a52cd25cf06f113f736
Junseok Oh <kotohana5706@edcan.kr>
2016-10-07 01:54:14 +0900

    [ShoppingCartActivity] Configure AppbarLayout, Activity Flow

--- app/src/main/java/kr/edcan/rerant/activity/ShoppingCartActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/ShoppingCartActivity.java
@@ -6,16 +6,39 @@
 
 package kr.edcan.rerant.activity;
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import kr.edcan.rerant.R;
+import kr.edcan.rerant.databinding.ActivityShoppingCartBinding;
 
 public class ShoppingCartActivity extends AppCompatActivity {
 
+
+    ActivityShoppingCartBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shopping_cart);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_shopping_cart);
+        initAppbarLayout();
+    }
+    private void initAppbarLayout() {
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setBackgroundColor(Color.WHITE);
+        binding.toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        getSupportActionBar().setTitle("예약할 주문 목록");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

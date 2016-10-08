b9af619a17cffa0edea76c0b0744b240a85d4934
Junseok Oh <kotohana5706@edcan.kr>
2016-10-08 22:11:02 +0900

    [ReserveCompleteActivity] Configure LayoutBinding, Default AppbarLayout Theme

--- app/src/main/java/kr/edcan/rerant/activity/ReserveCompleteActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/ReserveCompleteActivity.java
@@ -6,16 +6,37 @@
 
 package kr.edcan.rerant.activity;
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import kr.edcan.rerant.R;
+import kr.edcan.rerant.databinding.ActivityReserveCompleteBinding;
 
 public class ReserveCompleteActivity extends AppCompatActivity {
-
+    ActivityReserveCompleteBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reserve_complete);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reserve_complete);
+        initAppbarLayout();
+        setDefault();
+    }
+
+    private void setDefault() {
+    }
+
+    private void initAppbarLayout() {
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setBackgroundColor(Color.WHITE);
+        binding.toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        getSupportActionBar().setTitle("예약 완료");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }

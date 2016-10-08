6a25e74e39c9aadd054f38bec34145401971130c
Junseok Oh <kotohana5706@edcan.kr>
2016-10-09 02:33:55 +0900

    [ReserveExecuteActivity] Layout

--- app/src/main/java/kr/edcan/rerant/activity/ReserveExecuteActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/ReserveExecuteActivity.java
@@ -6,16 +6,52 @@
 
 package kr.edcan.rerant.activity;
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.MenuItem;
+
+import com.github.nitrico.lastadapter.BR;
+import com.github.nitrico.lastadapter.LastAdapter;
+
+import java.util.ArrayList;
 
 import kr.edcan.rerant.R;
+import kr.edcan.rerant.databinding.ActivityReserveCompleteBinding;
+import kr.edcan.rerant.databinding.ActivityReserveExecuteBinding;
+import kr.edcan.rerant.model.Menu;
+import kr.edcan.rerant.model.ReserveFooter;
 
 public class ReserveExecuteActivity extends AppCompatActivity {
 
+    ActivityReserveExecuteBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reserve_execute);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reserve_execute);
+        initAppbarLayout();
+        setDefault();
+    }
+
+    private void setDefault() {
+    }
+
+    private void initAppbarLayout() {
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setBackgroundColor(Color.WHITE);
+        binding.toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        getSupportActionBar().setTitle("예약하기");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

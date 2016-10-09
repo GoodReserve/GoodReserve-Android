9dc72aacb25a8832c9816f8a2ad3046589a51897
Junseok Oh <kotohana5706@edcan.kr>
2016-10-09 23:51:47 +0900

    [ReserveExeucuteActivity] Configure CardView Click Event, Slider slide event

--- app/src/main/java/kr/edcan/rerant/activity/ReserveExecuteActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/ReserveExecuteActivity.java
@@ -10,11 +10,14 @@ import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.github.nitrico.lastadapter.BR;
 import com.github.nitrico.lastadapter.LastAdapter;
+import com.rey.material.widget.Slider;
 
 import java.util.ArrayList;
 
@@ -24,9 +27,14 @@ import kr.edcan.rerant.databinding.ActivityReserveExecuteBinding;
 import kr.edcan.rerant.model.Menu;
 import kr.edcan.rerant.model.ReserveFooter;
 
-public class ReserveExecuteActivity extends AppCompatActivity {
+public class ReserveExecuteActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityReserveExecuteBinding binding;
+    CardView payNow, payLater;
+
+    int colorPrimary, personCount = 0;
+    boolean isPayNow = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +44,25 @@ public class ReserveExecuteActivity extends AppCompatActivity {
     }
 
     private void setDefault() {
+        colorPrimary = getResources().getColor(R.color.colorPrimary);
+        payLater = binding.reserveByCash;
+        payNow = binding.reserveByCard;
+        payNow.setOnClickListener(this);
+        payLater.setOnClickListener(this);
+        setPayLayout(true);
+        setPersonCount(1);
+        binding.reserveDatePickerExecute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // DatePickerDialog0
+            }
+        });
+        binding.reservePersonSlider.setOnPositionChangeListener(new Slider.OnPositionChangeListener() {
+            @Override
+            public void onPositionChanged(Slider view, boolean fromUser, float oldPos, float newPos, int oldValue, int newValue) {
+                setPersonCount(newValue);
+            }
+        });
     }
 
     private void initAppbarLayout() {
@@ -46,12 +73,40 @@ public class ReserveExecuteActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void setPayLayout(boolean payNow) {
+        isPayNow = payNow;
+        binding.reserveByCard.setCardBackgroundColor((payNow) ? colorPrimary : Color.WHITE);
+        binding.reserveByCash.setCardBackgroundColor((payNow) ? Color.WHITE : colorPrimary);
+        binding.reserveByCardTitle.setTextColor((payNow) ? Color.WHITE : colorPrimary);
+        binding.reserveByCardSubTitle.setTextColor((payNow) ? Color.WHITE : colorPrimary);
+        binding.reserveByCashTitle.setTextColor((payNow) ? colorPrimary : Color.WHITE);
+        binding.reserveByCashSubTitle.setTextColor((payNow) ? colorPrimary : Color.WHITE);
+    }
+    public void setPersonCount(int newValue){
+        binding.reservePersonCount.setText(newValue+"명");
+        personCount = newValue;
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.reserveByCard:
+                setPayLayout(true);
+                break;
+            case R.id.reserveByCash:
+                setPayLayout(false);
+                break;
+
+        }
+    }
+
+
 }

d62351bdff74b66dd4d7db44dfe9d38b8292f945
Junseok Oh <kotohana5706@edcan.kr>
2016-10-10 00:28:48 +0900

    [ReserveCompleteActivity] Add Search @Toolbar

--- app/src/main/java/kr/edcan/rerant/activity/ReserveSearchActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/ReserveSearchActivity.java
@@ -10,6 +10,8 @@ import android.databinding.DataBindingUtil;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import kr.edcan.rerant.R;
 import kr.edcan.rerant.databinding.ActivityReserveSearchBinding;
@@ -17,17 +19,35 @@ import kr.edcan.rerant.databinding.ActivityReserveSearchBinding;
 public class ReserveSearchActivity extends AppCompatActivity {
 
     ActivityReserveSearchBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_reserve_search);
         initAppbarLayout();
     }
+
     private void initAppbarLayout() {
         setSupportActionBar(binding.toolbar);
         binding.toolbar.setBackgroundColor(Color.WHITE);
         binding.toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
         getSupportActionBar().setTitle("예약하기");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

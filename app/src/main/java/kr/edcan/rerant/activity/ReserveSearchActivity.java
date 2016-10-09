09357e6644976618fd8582350751849c8fd51a06
Junseok Oh <kotohana5706@edcan.kr>
2016-10-10 00:21:31 +0900

    [ReserveSearchActivity] Implement Scrolling Behaviour to RecyclerView, Tag Layout

--- app/src/main/java/kr/edcan/rerant/activity/ReserveSearchActivity.java
+++ app/src/main/java/kr/edcan/rerant/activity/ReserveSearchActivity.java
@@ -6,16 +6,28 @@
 
 package kr.edcan.rerant.activity;
 
+import android.databinding.DataBindingUtil;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import kr.edcan.rerant.R;
+import kr.edcan.rerant.databinding.ActivityReserveSearchBinding;
 
 public class ReserveSearchActivity extends AppCompatActivity {
 
+    ActivityReserveSearchBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reserve_search);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reserve_search);
+        initAppbarLayout();
+    }
+    private void initAppbarLayout() {
+        setSupportActionBar(binding.toolbar);
+        binding.toolbar.setBackgroundColor(Color.WHITE);
+        binding.toolbar.setTitleTextColor(getResources().getColor(R.color.colorPrimary));
+        getSupportActionBar().setTitle("예약하기");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }

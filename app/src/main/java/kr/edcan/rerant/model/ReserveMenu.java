8fc82c2a61ad6852f5bb43d74d0eb7c318f62385
Junseok Oh <kotohana5706@edcan.kr>
2016-10-11 12:12:36 +0900

    [ReserveMenu/Models] Implement ReserveMenu Model

---
+++ app/src/main/java/kr/edcan/rerant/model/ReserveMenu.java
@@ -0,0 +1,26 @@
+package kr.edcan.rerant.model;
+
+/**
+ * Created by JunseokOh on 2016. 10. 11..
+ */
+public class ReserveMenu {
+    private String imageUrl, title, money;
+
+    public ReserveMenu(String imageUrl, String title, String money) {
+        this.imageUrl = imageUrl;
+        this.title = title;
+        this.money = money;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+}

a49c01b326bdd585a6af7746ec614d97d39b28bf
Junseok Oh <kotohana5706@edcan.kr>
2016-10-15 23:56:11 +0900

    [NetworkInterface] Update REST APi

---
+++ app/src/main/java/kr/edcan/rerant/model/Bucket.java
@@ -0,0 +1,7 @@
+package kr.edcan.rerant.model;
+
+/**
+ * Created by Junseok on 2016-10-15.
+ */
+public class Bucket {
+}
--- app/src/main/java/kr/edcan/rerant/utils/NetworkInterface.java
+++ app/src/main/java/kr/edcan/rerant/utils/NetworkInterface.java
@@ -6,7 +6,15 @@
 
 package kr.edcan.rerant.utils;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+import kr.edcan.rerant.model.Bucket;
+import kr.edcan.rerant.model.Menu;
+import kr.edcan.rerant.model.Reservation;
+import kr.edcan.rerant.model.Restaurant;
 import kr.edcan.rerant.model.User;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
@@ -18,6 +26,8 @@ import retrofit2.http.Query;
  * Created by JunseokOh on 2016. 8. 11..
  */
 public interface NetworkInterface {
+
+    // Authenticate
 
     @GET("/auth/facebook/token")
     Call<User> facebookLogin(@Query("access_token") String token);
@@ -35,5 +45,49 @@ public interface NetworkInterface {
     Call<User> registerUser(@Field("name") String name, @Field("phone") String phoneNum,
                             @Field("password") String password, @Field("password_chk") String passwordChk, @Field("email") String email);
 
+    // Restaurant Data
+    @POST("/rest/list")
+    Call<ArrayList<Restaurant>> getRestaurantList();
+
+    @POST("/rest/search")
+    @FormUrlEncoded
+    Call<Restaurant> getRestaurantInfo(@Field("id") String restaurantId);
+
+    @POST("/rest/search/name")
+    @FormUrlEncoded
+    Call<ArrayList<Restaurant>> getRestaurantListByWord(@Field("query") String query);
+
+    @POST("/rest/search/tag")
+    @FormUrlEncoded
+    Call<ArrayList<Restaurant>> getRestaurantListByTag(@Field("query") String query);
+
+    // Reservation
+    @POST("/resv/add")
+    @FormUrlEncoded
+    Call<Reservation> generateReservation(@Field("reservation_id") String reservationId, @Field("reservation_marker") String _id,
+                                          @Field("reservation_time") Date time, @Field("people") int peopleCount, @Field("reservation_payment") int payment,
+                                          @Field("reservation_menu") String bucketId, @Field("reservation_price") int totalMoney);
+    @POST("/resv/cancel")
+    @FormUrlEncoded
+    Call<ResponseBody> cancelReservation(@Field("reservation_id") String reservationId, @Field("cancel_reason") int cancelReason,
+                                         @Field("cancel_comment") String cancelComment);
+
+    // Get Menu
+    @POST("/menu/rest/list")
+    @FormUrlEncoded
+    Call<ArrayList<Menu>> getMenuList(@Field("restaurant_id") String restaurantId);
+
+    // Bucket
+    @POST("/bucket/add")
+    @FormUrlEncoded
+    Call<Bucket> newBucket(@Field("menus") ArrayList<Menu> menus);
+
+    @POST("/bucket/update")
+    @FormUrlEncoded
+    Call<Bucket> updateBucket(@Field("bucket_id") String bucketId, @Field("menus") ArrayList<Menu> menus);
+
+    @POST("/bucket/info")
+    @FormUrlEncoded
+    Call<Bucket> getBucketInfo(@Field("bucket_id") String bucketId);
 
 }

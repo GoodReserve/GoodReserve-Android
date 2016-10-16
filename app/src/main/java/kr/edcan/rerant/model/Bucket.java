3a3e376bb494bd7ffe15dc7f2488f1a459c3cff0
Junseok Oh <kotohana5706@edcan.kr>
2016-10-16 13:39:37 +0900

    [Bucket/Model] Add Restaurant Id for Local Save

--- app/src/main/java/kr/edcan/rerant/model/Bucket.java
+++ app/src/main/java/kr/edcan/rerant/model/Bucket.java
@@ -5,10 +5,20 @@ package kr.edcan.rerant.model;
  */
 public class Bucket {
     private String _id, menus;
-
+    private String restaurantId;
     public Bucket(String _id, String menus) {
         this._id = _id;
         this.menus = menus;
+    }
+
+    public Bucket(String _id, String menus, String restaurantId) {
+        this._id = _id;
+        this.menus = menus;
+        this.restaurantId = restaurantId;
+    }
+
+    public String getRestaurantId() {
+        return restaurantId;
     }
 
     public String get_id() {
@@ -17,5 +27,17 @@ public class Bucket {
 
     public String getMenus() {
         return menus;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public void setMenus(String menus) {
+        this.menus = menus;
+    }
+
+    public void setRestaurantId(String restaurantId) {
+        this.restaurantId = restaurantId;
     }
 }
